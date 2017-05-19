760886ff29b2425b96f316ac8f55f562ab33c96e
GHULAM MOINUL QUADIR <ghulam6686@gmail.com>
2017-05-19 10:13:17 +0530

    Update PostToFacebook.java

--- PostingQuotesAPI/src/main/java/org/jugnoo/assignment/PostingQuotesAPI/postToFacebook/PostToFacebook.java
+++ PostingQuotesAPI/src/main/java/org/jugnoo/assignment/PostingQuotesAPI/postToFacebook/PostToFacebook.java
@@ -1,3 +1,10 @@
+/*
+
+this class is for posting the image to the user's timeline
+
+*/
+
+
 package org.jugnoo.assignment.PostingQuotesAPI.postToFacebook;
 
 import java.awt.Color;
@@ -31,35 +38,47 @@ import com.restfb.types.User;
 public class PostToFacebook {
 
 	@GET
-    @Produces(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_XML)//this produces response as xml 
 	public List<Quote> postToFacebook() throws IOException{
-		
-		
-		String message = "Sample";
-        QuoteResource quoteResource=new QuoteResource();
+	String message = "Sample";//This contains the actual quote String
+        QuoteResource quoteResource=new QuoteResource();//calling for the new quote
         message=quoteResource.getQuote()+"\n  -Dr. A.P.J. Abdul Kalam";
 		
-		
-		BufferedImage bufferedImage = new BufferedImage(700, 400,
-                BufferedImage.TYPE_INT_RGB);
+	//drawing the image	
+	BufferedImage bufferedImage = new BufferedImage(700, 400,BufferedImage.TYPE_INT_RGB);
         Graphics graphics = bufferedImage.getGraphics();
         graphics.setColor(Color.RED);
         graphics.fillRect(0, 0, 700, 400);
         graphics.setColor(Color.BLACK);
         graphics.setFont(new Font("Arial Black", Font.BOLD, 10));
         
+	//writing the Quote on the image
         graphics.drawString(message, 10, 180);
+	
+	//savig the image in file system so that we can fetch this whle posting it to facebook
         ImageIO.write(bufferedImage, "jpg", new File("image.jpg"));
-        FileInputStream fis=new FileInputStream(new File("image.jpg"));
+        
+	//taking the saved image as input
+	FileInputStream fis=new FileInputStream(new File("image.jpg"));
+	
+	//taking the access token to access the user's profile for posting the image
         String accessToken="EAACEdEose0cBADOD91jDSjWVlYhclBnug1qOW0qlZB6PZCaOGikiEGgwhZBNLHb52zo0abSxv5LRACLZCl6tkW8RHUaZBr7FfKB6Uuq7Hf00XWZA0mppGvmOiADNe4IVrcmicAcDqCL0BnUSh8hF7I5enslvMPwuuilcfqVGbCHxvNrGTSLYU0vdLrvO9eGzYZD";
         FacebookClient facebookClient=new DefaultFacebookClient(accessToken);
+	
+	//Posting the image to user's tmeline
         FacebookType facebookType=facebookClient.publish("me/photos", FacebookType.class, BinaryAttachment.with("image.jpg", fis), Parameter.with("Message", "This is for Test"));
-		User user=facebookClient.fetchObject("me", User.class);
-		QuoteService quoteService=new QuoteService();
-		Quote quote = quoteService.getQuote(facebookType, user,message);
-		List<Quote> quotes=new ArrayList<>();
-		quotes.add(quote);
-		//quotes.add(new Quote("","","","",new Date()));
+	
+	//getting the user's information to whom the image is posted
+	User user=facebookClient.fetchObject("me", User.class);
+	
+	
+	QuoteService quoteService=new QuoteService();
+	Quote quote = quoteService.getQuote(facebookType, user,message);
+	List<Quote> quotes=new ArrayList<>();
+	quotes.add(quote);
+	
+	//returning the list of quotes in xml formate.
+	//actually it is always one because there is only one post i am posting at atime
     	return quotes;
 	}
 }
