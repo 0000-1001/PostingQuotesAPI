32b21c3c9e0a73586774eff327fd0e191d4fc58c
GHULAM MOINUL QUADIR <ghulam6686@gmail.com>
2017-05-19 10:20:09 +0530

    Update QuoteResource.java

--- PostingQuotesAPI/src/main/java/org/jugnoo/assignment/PostingQuotesAPI/resources/QuoteResource.java
+++ PostingQuotesAPI/src/main/java/org/jugnoo/assignment/PostingQuotesAPI/resources/QuoteResource.java
@@ -1,3 +1,8 @@
+/*
+This class works as Quote Resource where quotes are created and returned when needed
+*/
+
+
 package org.jugnoo.assignment.PostingQuotesAPI.resources;
 
 import java.util.ArrayList;
@@ -11,9 +16,11 @@ import javax.ws.rs.core.MediaType;
 
 @Path("quotes")
 public class QuoteResource {
-	public List<String> quotes;
-	public QuoteResource(){
-		quotes=new ArrayList<>();
+	public List<String> quotes;//List of quotes
+	public QuoteResource(){//constructor for QuoteResource
+		quotes=new ArrayList<>();//creating new Arraylist that contains the list of quotes available
+		
+		//adding the quotes to the list
 		quotes.add("Failure will never overtake me if my determination to succeed is strong enough.");
 		quotes.add("Don’t take rest after your first victory because if you fail in second, more lips are waiting to say that your first victory was just luck.");
 		quotes.add("All Birds find shelter during a rain. But Eagle avoids rain by flying above the Clouds.");
@@ -24,6 +31,8 @@ public class QuoteResource {
 		quotes.add("Thinking should become your capital asset, no matter whatever ups and downs you come across in your life.");
 		quotes.add("Without your involvement you can't succeed. With your involvement you can't fail.");
 	}
+	
+	//this function is user for returning a single quote when needed
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getQuote(){
